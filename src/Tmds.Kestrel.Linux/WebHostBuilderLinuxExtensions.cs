6abb112d09fe8cf0fc2680e3ee40c3e2c6d11fe4
Tom Deseyn <tom.deseyn@gmail.com>
2017-04-03 20:42:15 +0200

    Add WebHostBuilderLinuxExtensions

---
+++ src/Tmds.Kestrel.Linux/WebHostBuilderLinuxExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Server.Kestrel.Transport;
+using Microsoft.Extensions.DependencyInjection;
+using Tmds.Kestrel.Linux;
+
+namespace Microsoft.AspNetCore.Hosting
+{
+    public static class WebHostBuilderLibuvExtensions
+    {
+        public static IWebHostBuilder UseLinuxTransport(this IWebHostBuilder hostBuilder)
+        {
+            return hostBuilder.ConfigureServices(services =>
+            {
+                services.AddSingleton<ITransportFactory, TransportFactory>();
+            });
+        }
+
+        public static IWebHostBuilder UseLinuxTransport(this IWebHostBuilder hostBuilder, Action<TransportOptions> options)
+        {
+            return hostBuilder.UseLinuxTransport().ConfigureServices(services =>
+            {
+                services.Configure(options);
+            });
+        }
+    }
+}
