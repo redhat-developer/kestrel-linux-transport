2d15d4c7f315cf24fe8bb129a44cb22598c14890
Tom Deseyn <tom.deseyn@gmail.com>
2017-12-15 11:40:14 +0100

    Fix clang errors

--- src/RedHatX.AspNetCore.Server.Kestrel.Transport.Linux.Native/socket.cpp
+++ src/RedHatX.AspNetCore.Server.Kestrel.Transport.Linux.Native/socket.cpp
@@ -985,6 +985,9 @@ PosixResult RHXKL_Duplicate(intptr_t socket, intptr_t* dup)
     return ToPosixResult(rv);
 }
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas" // Fix next ignore being unknown
+#pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant" // expanded from macro 'CMSG_FIRSTHDR'
 PosixResult RHXKL_ReceiveHandle(intptr_t socket, intptr_t* receiveHandle, int32_t blocking)
 {
     if (receiveHandle == nullptr)
@@ -1019,7 +1022,7 @@ PosixResult RHXKL_ReceiveHandle(intptr_t socket, intptr_t* receiveHandle, int32_
 
     if (rv != -1)
     {
-        for (struct cmsghdr* cmsg = CMSG_FIRSTHDR(&header); cmsg != NULL; cmsg = CMSG_NXTHDR(&header,cmsg))
+        for (struct cmsghdr* cmsg = CMSG_FIRSTHDR(&header); cmsg != nullptr; cmsg = CMSG_NXTHDR(&header,cmsg))
         {
             if (cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SCM_RIGHTS)
             {
@@ -1089,6 +1092,7 @@ PosixResult RHXKL_AcceptAndSendHandleTo(intptr_t acceptSocket, intptr_t toSocket
 
     return ToPosixResult(rv);
 }
+#pragma clang diagnostic pop
 
 PosixResult RHXKL_SocketPair(int32_t addressFamily, int32_t socketType, int32_t protocolType, int32_t blocking, intptr_t* socket1, intptr_t* socket2)
 {
