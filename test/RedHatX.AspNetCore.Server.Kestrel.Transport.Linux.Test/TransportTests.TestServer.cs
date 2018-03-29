b48372eaba4b7c9357a6e7be1c1f1f92389faca7
Tom Deseyn <tom.deseyn@gmail.com>
2018-03-29 10:37:47 +0200

    Sync with upstream

--- test/RedHatX.AspNetCore.Server.Kestrel.Transport.Linux.Test/TransportTests.TestServer.cs
+++ test/RedHatX.AspNetCore.Server.Kestrel.Transport.Linux.Test/TransportTests.TestServer.cs
@@ -28,6 +28,19 @@ namespace Tests
         public bool AioReceive { get; set; } = false;
     }
 
+    internal class DuplexPipe : IDuplexPipe
+    {
+        public DuplexPipe(PipeReader reader, PipeWriter writer)
+        {
+            Input = reader;
+            Output = writer;
+        }
+
+        public PipeReader Input { get; }
+
+        public PipeWriter Output { get; }
+    }
+
     class TestServer : IConnectionDispatcher, IDisposable
     {
         private Transport _transport;
