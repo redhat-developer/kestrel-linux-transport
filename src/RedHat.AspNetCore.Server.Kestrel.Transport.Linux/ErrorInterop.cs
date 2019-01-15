6d0a3950c53b944239a71b571cc18f8a004e609e
Tom Deseyn <tom.deseyn@gmail.com>
2019-01-15 17:34:30 +0100

    Use Tmds.LibC errno

--- src/RedHat.AspNetCore.Server.Kestrel.Transport.Linux/ErrorInterop.cs
+++ src/RedHat.AspNetCore.Server.Kestrel.Transport.Linux/ErrorInterop.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Runtime.InteropServices;
+using static Tmds.LibC.Definitions;
 
 namespace RedHat.AspNetCore.Server.Kestrel.Transport.Linux
 {
@@ -13,25 +14,8 @@ namespace RedHat.AspNetCore.Server.Kestrel.Transport.Linux
         {
             int maxBufferLength = 1024; // should be long enough for most any UNIX error
             byte* buffer = stackalloc byte[maxBufferLength];
-            byte* message = StrErrorR(errno, buffer, maxBufferLength);
-
-            if (message == null)
-            {
-                // This means the buffer was not large enough, but still contains
-                // as much of the error message as possible and is guaranteed to
-                // be null-terminated. We're not currently resizing/retrying because
-                // maxBufferLength is large enough in practice, but we could do
-                // so here in the future if necessary.
-                message = buffer;
-            }
-
-            return Marshal.PtrToStringAnsi((IntPtr)message);
+            int rv = Tmds.LibC.Definitions.strerror_r(errno, buffer, maxBufferLength);
+            return rv == 0 ? Marshal.PtrToStringAnsi((IntPtr)buffer) : $"errno={errno}";
         }
-
-        [DllImport(Interop.Library, EntryPoint = "RHXKL_StrErrorR")]
-        private static extern unsafe byte* StrErrorR(int errno, byte* buffer, int bufferSize);
-
-        [DllImport(Interop.Library, EntryPoint = "RHXKL_GetErrnoValues")]
-        public static extern void GetErrnoValues(int[] knownErrorValues, int count);
     }
 }
