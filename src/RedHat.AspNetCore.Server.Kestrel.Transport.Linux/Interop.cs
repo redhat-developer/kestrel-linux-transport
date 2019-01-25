8c89246c6e766c9211c262aa9cdc2fa49e63b3ba
Tom Deseyn <tom.deseyn@gmail.com>
2019-01-25 10:08:39 +0100

    Use Tmds.LibC sched

--- src/RedHat.AspNetCore.Server.Kestrel.Transport.Linux/Interop.cs
+++
@@ -1,7 +0,0 @@
-namespace RedHat.AspNetCore.Server.Kestrel.Transport.Linux
-{
-    static class Interop
-    {
-        public const string Library = "RedHat.AspNetCore.Server.Kestrel.Transport.Linux.Native";
-    }
-}
