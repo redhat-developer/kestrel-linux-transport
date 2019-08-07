a87885a1ed20828a62c804f74dcdfac748c4fc95
Stephen Halter <halter73@gmail.com>
2019-08-07 19:21:28 +0000

    copy rest of TransportConnection

--- src/RedHat.AspNetCore.Server.Kestrel.Transport.Linux/TransportConnection.cs
+++ src/RedHat.AspNetCore.Server.Kestrel.Transport.Linux/TransportConnection.cs
@@ -78,6 +78,39 @@ namespace RedHat.AspNetCore.Server.Kestrel.Transport.Linux
         }
     }
 
+    internal partial class TransportConnection : IConnectionIdFeature,
+                                                 IConnectionTransportFeature,
+                                                 IConnectionItemsFeature,
+                                                 IMemoryPoolFeature,
+                                                 IConnectionLifetimeFeature
+    {
+        // NOTE: When feature interfaces are added to or removed from this TransportConnection class implementation,
+        // then the list of `features` in the generated code project MUST also be updated.
+        // See also: tools/CodeGenerator/TransportConnectionFeatureCollection.cs
+
+        MemoryPool<byte> IMemoryPoolFeature.MemoryPool => MemoryPool;
+
+        IDuplexPipe IConnectionTransportFeature.Transport
+        {
+            get => Transport;
+            set => Transport = value;
+        }
+
+        IDictionary<object, object> IConnectionItemsFeature.Items
+        {
+            get => Items;
+            set => Items = value;
+        }
+
+        CancellationToken IConnectionLifetimeFeature.ConnectionClosed
+        {
+            get => ConnectionClosed;
+            set => ConnectionClosed = value;
+        }
+
+        void IConnectionLifetimeFeature.Abort() => Abort(new ConnectionAbortedException("The connection was aborted by the application via IConnectionLifetimeFeature.Abort()."));
+    }
+
     internal partial class TransportConnection : IFeatureCollection
     {
         private static readonly Type IConnectionIdFeatureType = typeof(IConnectionIdFeature);
